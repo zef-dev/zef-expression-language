81bd9b838c4b054a4e511622daabe7acaf70bbf4
tole <tole.car@gmail.com>
2020-09-19 20:17:52 +0200

    empty handling unit test

---
+++ tests/NotSetEvaluationTest.php
@@ -0,0 +1,61 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use Zef\Zel\Symfony\ExpressionLanguage;
+use Zef\Zel\ArrayResolver;
+
+class NotSetEvaluationTest extends TestCase
+{
+    /**
+     * @dataProvider resolveToNullProvider
+     */
+    public function testResolveToNull( $expression, array $values, $expected)
+    {
+        $expressionLanguage =   new ExpressionLanguage();
+        $resolver           =   new ArrayResolver( $values);
+        $values             =   $resolver->getValues();
+        
+        $this->assertEquals( $expected, $expressionLanguage->evaluate( $expression, $values));
+    }
+    
+    public function resolveToNullProvider()
+    {
+        return [
+            ['myvar', [], null],
+            ['!myvar', [], true],
+            ['myvar || true', [], true],
+            ['myarr.value', ['myarr' => []], true],
+            ['!myarr.value', ['myarr' => []], false],
+            ['myarr.value', ['myarr' => ['value' => true]], true],
+            ['!myarr.value', ['myarr' => ['value' => true]], false],
+            ['myarr.value', ['myarr' => ['valuex' => true]], null],
+            ['!myarr.value', ['myarr' => ['valuex' => true]], true],
+        ];
+    }
+
+    /**
+     * @dataProvider resolveToNullSymfonyProvider
+     */
+    public function testResolveToNullSymfony( $expression, array $values, $expected)
+    {
+        $expressionLanguage =   new ExpressionLanguage();
+//         $resolver           =   new ArrayResolver( $values);
+//         $values             =   $resolver->getValues();
+        
+        $this->assertEquals( $expected, $expressionLanguage->evaluate( $expression, $values));
+    }
+    
+    public function resolveToNullSymfonyProvider()
+    {
+        return [
+            ['myvar', [], null],
+            ['!myvar', [], true],
+            ['myvar || true', [], true],
+            ['myarr[\'value\']', ['myarr' => []], null],
+            ['!myarr[\'value\']', ['myarr' => []], true],
+        ];
+    }
+
+
+}
