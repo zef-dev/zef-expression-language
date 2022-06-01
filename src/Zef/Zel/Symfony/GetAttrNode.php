238f33f0188eb2c5f247797e1855d3477046cfae
mmarcec007 <mark.marcec@gmail.com>
2022-06-01 10:30:07 +0200

    Avoid calling methods apn properties which are not public

--- src/Zef/Zel/Symfony/GetAttrNode.php
+++ src/Zef/Zel/Symfony/GetAttrNode.php
@@ -24,7 +24,11 @@ class GetAttrNode extends \Symfony\Component\ExpressionLanguage\Node\GetAttrNode
                 }
                 
                 $property = $this->nodes['attribute']->attributes['value'];
-                
+
+                if (!$this->_shouldCallMethod($this->attributes['type'], $obj, $property)) {
+                    return null;
+                }
+
                 return $obj->$property;
                 
             case self::METHOD_CALL:
@@ -41,6 +45,10 @@ class GetAttrNode extends \Symfony\Component\ExpressionLanguage\Node\GetAttrNode
                     throw new \RuntimeException(sprintf('Unable to call method "%s" of object "%s".', $this->nodes['attribute']->attributes['value'], \get_class($obj)));
                 }
                 
+                if (!$this->_shouldCallMethod($this->attributes['type'], $obj, $method)) {
+                    return null;
+                }
+
                 return $toCall(...array_values($this->nodes['arguments']->evaluate($functions, $values)));
                 
             case self::ARRAY_CALL:
@@ -57,5 +65,39 @@ class GetAttrNode extends \Symfony\Component\ExpressionLanguage\Node\GetAttrNode
                 return $array[$this->nodes['attribute']->evaluate($functions, $values)] ?? null;
         }
     }
-    
+
+    private function _shouldCallMethod($callType, $object, $value) {
+        $isAccessible = 0;
+        switch ($callType) {
+            case self::PROPERTY_CALL:
+                if (in_array($value, array_keys(get_object_vars($object)))) {
+                    $isAccessible++;
+                }
+
+                if (is_a($object, 'Zef\Zel\IValueAdapter') && in_array($value, array_keys(get_object_vars($object->get())))) {
+                    $isAccessible++;
+                }
+
+                break;
+            case self::METHOD_CALL:
+                if (in_array($value, get_class_methods($object))) {
+                    $isAccessible++;
+                }
+
+                if (is_a( $object, 'Zef\Zel\IValueAdapter') && in_array($value, get_class_methods($object->get()))) {
+                    $isAccessible++;
+                }
+
+                break;
+            default:
+                break;
+        }
+
+        if (empty($isAccessible)) {
+            return false;
+        }
+
+        return true;
+    }
+
 }
